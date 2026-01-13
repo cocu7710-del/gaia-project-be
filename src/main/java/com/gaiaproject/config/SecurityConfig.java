4476941d382ae04134463936b1729e318ee02110
[softworks]한동민 <cocu7712@softworks.co.kr>
2026-01-13 18:41:03 +0900

    init Setting

--- src/main/java/com/gaiaproject/config/SecurityConfig.java
+++ src/main/java/com/gaiaproject/config/SecurityConfig.java
@@ -1,18 +1,21 @@
-package com.example.gaiaproject.config;
+package com.gaiaproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         // Swagger/OpenAPI는 인증 없이 접근 허용
                         .requestMatchers(
